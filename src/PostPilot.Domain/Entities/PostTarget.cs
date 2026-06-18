432bf49da8c8eaa375f1b31dfa731c433872350e
Tantai07 <167741192+Tantai07@users.noreply.github.com>
2026-06-18 16:53:43 +0700

    Add post target constructor

--- src/PostPilot.Domain/Entities/PostTarget.cs
+++ src/PostPilot.Domain/Entities/PostTarget.cs
@@ -5,6 +5,17 @@ namespace PostPilot.Domain.Entities;
 
 public sealed class PostTarget : SoftDeleteEntity
 {
+    private PostTarget()
+    {
+    }
+
+    public PostTarget(Guid postId, PostTargetPlatform targetPlatform, Guid targetAccountId)
+    {
+        PostId = postId;
+        TargetPlatform = targetPlatform;
+        TargetAccountId = targetAccountId;
+    }
+
     public Guid PostId { get; private set; }
     public PostTargetPlatform TargetPlatform { get; private set; }
     public Guid TargetAccountId { get; private set; }
