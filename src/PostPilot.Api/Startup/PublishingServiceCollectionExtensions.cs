e525de2a5d30e43a8eb72b756c8ca2be97aa137c
Tantai07 <167741192+Tantai07@users.noreply.github.com>
2026-06-18 17:50:02 +0700

    Register publish now command

--- src/PostPilot.Api/Startup/PublishingServiceCollectionExtensions.cs
+++ src/PostPilot.Api/Startup/PublishingServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 using PostPilot.Api.Features.Publishing;
+using PostPilot.Api.Features.Publishing.Commands;
 
 namespace PostPilot.Api.Startup;
 
@@ -7,6 +8,7 @@ public static class PublishingServiceCollectionExtensions
     public static IServiceCollection AddPublishingFeature(this IServiceCollection services)
     {
         services.AddScoped<IPostPublisher, MockPostPublisher>();
+        services.AddScoped<PublishPostCommandExecutor>();
 
         return services;
     }
