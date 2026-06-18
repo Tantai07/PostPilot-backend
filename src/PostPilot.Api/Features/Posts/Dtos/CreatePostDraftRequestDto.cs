b7bdbf770522eaae9c7ed51315fe3be263575da3
Tantai07 <167741192+Tantai07@users.noreply.github.com>
2026-06-18 16:55:13 +0700

    Add create post draft request dto

---
+++ src/PostPilot.Api/Features/Posts/Dtos/CreatePostDraftRequestDto.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PostPilot.Api.Features.Posts.Dtos;
+
+public sealed class CreatePostDraftRequestDto
+{
+    public Guid? CategoryId { get; set; }
+
+    [Required]
+    [MaxLength(4096)]
+    public string Caption { get; set; } = string.Empty;
+
+    public List<Guid> MediaIds { get; set; } = [];
+
+    public List<string> TargetPlatforms { get; set; } = [];
+}
