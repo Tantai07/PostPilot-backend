72668b309db8909ab9f0305b7d045e9b3ea0ccbe
Tantai07 <167741192+Tantai07@users.noreply.github.com>
2026-06-18 16:23:52 +0700

    Import category mapping helpers

--- src/PostPilot.Api/Features/Categories/Commands/CreateCategoryCommandExecutor.cs
+++ src/PostPilot.Api/Features/Categories/Commands/CreateCategoryCommandExecutor.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using PostPilot.Api.Features.Categories;
 using PostPilot.Api.Features.Categories.Dtos;
 using PostPilot.Domain.Entities;
 using PostPilot.Infrastructure.Database;
